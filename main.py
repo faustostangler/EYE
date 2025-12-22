206a8eef25c44c3387ff02c0a3cf73368fcf8907
Fausto Stangler (Linux) <fausto.stangler@gmail.com>
2025-12-21 22:46:38 -0300

    no compose debug

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import sys
+
+print("Hello from main.py (top level)", file=sys.stderr)
+print("Hello from main.py (top level - stdout)", file=sys.stdout)
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
+    print("Health check called!", file=sys.stderr)
     return {"status": "ok"}
